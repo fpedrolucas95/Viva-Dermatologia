3ee0d8ac5180f714b9a471d43be2df2c7a930e27
Pedro Lucas França <73343509+fpedrolucas95@users.noreply.github.com>
2023-06-01 11:56:50 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -138,15 +138,87 @@ function atualizarParcelas() {
   let divDinamica = document.getElementById("divDinamica");
 }
 
-document.getElementById("adicionar").addEventListener("click", function () {
+// Função que verifica se todos os campos e opções de seleção estão preenchidos
+function verificarPreenchimento() {
   let nome = document.getElementById("nome");
   let procedimento = document.getElementById("procedimento").value;
   let valor = parseFloat(document.getElementById("valor").value);
   let quantidade = parseInt(document.getElementById("quantidade").value);
+  let parcelas = document.getElementById("parcelas").value;
 
-  // Verifica se nenhum dos campos está vazio
-  if (!nome.value || !procedimento || !valor || !quantidade) {
-    alert("Por favor, preencha todos os campos");
-    return;
+  // Verifica se nenhum dos campos ou a opção de seleção está vazia
+  if (!nome.value || !procedimento || !valor || !quantidade || !parcelas) {
+    alert(
+      "Por favor, preencha todos os campos e selecione uma opção em parcelas"
+    );
+    return false;
+  }
+  return true;
+}
+
+// Declarando 'janelaDeImpressao' no escopo global para que possa ser acessada por outras funções
+let janelaDeImpressao;
+
+// Função para imprimir a divDinamica
+function imprimirDivDinamica() {
+  let nome = document.getElementById("nome").value;
+  let conteudo = `<div style='display: flex; flex-direction: column; justify-content: space-between; height: 550px; padding-top: 266px; padding-right: 20px;'><div style='flex: 1; display: flex; flex-direction: column; justify-content: flex-start;'>`;
+  conteudo += `<p style='font-size: 24px; font-weight: bold;'>${nome},</p>`;
+  conteudo +=
+    "<p style='font-size: 16px;'>Plano de tratamento:</p></div><div style='flex: 1; display: flex; flex-direction: column; justify-content: center;'>";
+
+  adicoes.forEach(function (adicao, index) {
+    conteudo += `<div style='display: flex; justify-content: space-between; width: 760px; padding: 0 10px;'><span style='width: 48px;'>${
+      index + 1
+    }.</span><span style='width: 500px;'>${
+      adicao.procedimento
+    }</span><span style='width: 48px;'>${
+      adicao.quantidade
+    }</span><span style='width: 96px;'>${formatarComoMoeda(
+      adicao.valor
+    )}</span></div><br>`;
+  });
+
+  conteudo +=
+    "</div><div style='flex: 1; display: flex; flex-direction: column; justify-content: flex-end;'>";
+
+  let parcelas = document.getElementById("parcelas");
+  let numParcelas = parseInt(parcelas.value);
+  let valorParcela = somaTotal / numParcelas;
+
+  conteudo += `<div style="text-align: right;"><p>Valor Total: ${formatarComoMoeda(
+    somaTotal
+  )}</p><p>Ou em ${numParcelas}x de ${formatarComoMoeda(
+    valorParcela
+  )} no cartão de crédito, ou, R$ ${formatarComoMoeda(
+    somaTotal
+  )} para pagamento à vista.</p><p>Brasília, ${new Date().toLocaleDateString(
+    "pt-BR",
+    { day: "2-digit", month: "long", year: "numeric" }
+  )}</p><p>Orçamento válido por 60 dias.</p></div></div></div>`;
+
+  janelaDeImpressao = window.open("", "", "height=1280,width=793");
+  janelaDeImpressao.document.write("<html><head><title>Imprimir</title>");
+  janelaDeImpressao.document.write("</head><body >");
+  janelaDeImpressao.document.write(conteudo);
+  janelaDeImpressao.document.write("</body></html>");
+  janelaDeImpressao.document.close();
+  janelaDeImpressao.print();
+}
+
+// Evento de clique para imprimir a divDinamica
+document.getElementById("imprimir").addEventListener("click", function () {
+  if (verificarPreenchimento()) {
+    console.log("Imprimindo...");
+    imprimirDivDinamica();
+  }
+});
+
+// Evento de clique para cancelar a impressão
+document.getElementById("cancelar").addEventListener("click", function () {
+  // Verifica se a janela de impressão existe antes de tentar fechá-la
+  if (janelaDeImpressao) {
+    janelaDeImpressao.close();
+    janelaDeImpressao.focus();
   }
 });
