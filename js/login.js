5911de36c476664e06a4608656d83813e836fe0e
Pedro Lucas França <73343509+fpedrolucas95@users.noreply.github.com>
2023-07-03 18:59:14 -0300

    Lógica simples de login
    Ela não é segura, portanto, não a use como referência. Considerando que o projeto não possui dados sensíveis, ele atende bem ao propósito pretendido. A razão dessa lógica não é a proteção do aplicativo, que é apenas uma simples calculadora e um criador de orçamentos, mas sim regular quem está emitindo. Sendo assim, para esse projeto, não há a necessidade de um sistema de login complexo e robusto.

---
+++ js/login.js
@@ -0,0 +1,36 @@
+var users = {
+    "fabiana": {
+        password: "senhaFabiana",
+        name: "Fabiana Carvalho Mendes"
+    },
+    "evangela": {
+        password: "senhaEvangela",
+        name: "Evangela Evangelista da Silva"
+    },
+    "alice": {
+        password: "senhaAlice",
+        name: "Alice de Jesus Gonçalves"
+    },
+    "mabya": {
+        password: "senhaMabya",
+        name: "Mabya Campos"
+    }
+}
+
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Verifique se o usuário e a senha correspondem a algum dos usuários e senhas definidos acima
+    if(users[username] && users[username].password === password) {
+        // Se o usuário estiver autenticado, oculte o formulário de login e mostre a aplicação.
+        document.getElementById('divLogin').style.display = "none";
+        document.getElementById('principal').style.display = "block";
+        document.getElementById('resultado').style.display = "block";
+        
+        // Armazena o nome completo do usuário na variável 'userLogado' de 'main.js'
+        userLogado = users[username].name;
+    } else {
+        alert('Usuário ou senha incorretos. Por favor, tente novamente.');
+    }
+}
